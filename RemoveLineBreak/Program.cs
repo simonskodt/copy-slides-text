d57c2f29b1fa14560c4bd5fee58def904e1f452b
simonskodt <simon@skodt.dk>
2022-07-13 13:42:50 +0200

    feat(basic logic): only needs to copy the given text to the clipboard

---
+++ RemoveLineBreak/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+// Read slide text
+var input = Console.In.ReadToEnd();
+
+string cleaned = input.Replace("\n", " ").Replace("\r", "");
+
+string addNewLines = cleaned.Replace("\a", System.Environment.NewLine);
+
+Clipboard.SetDataObject(addNewLines);
+
+Console.WriteLine(addNewLines);
